edd7a84b169b83bb4cf49f3c05fb6ae08de9ed46
thegouge <thegouge42@gmail.com>
2018-12-05 16:43:22 -0800

    Updates

---
+++ precache-manifest.dcf3618d504a3fdaf99af2720da3a622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5de345f195acd1923e7d1f75ad95f7e9",
+    "url": "/fcc-markdown-preview/static/media/Sanic.5de345f1.jpg"
+  },
+  {
+    "revision": "682dc27351e6e53775ff",
+    "url": "/fcc-markdown-preview/static/js/runtime~main.682dc273.js"
+  },
+  {
+    "revision": "6b569b177d3b1c095f19",
+    "url": "/fcc-markdown-preview/static/js/main.6b569b17.chunk.js"
+  },
+  {
+    "revision": "6a71f2951fd788489a42",
+    "url": "/fcc-markdown-preview/static/js/1.6a71f295.chunk.js"
+  },
+  {
+    "revision": "6b569b177d3b1c095f19",
+    "url": "/fcc-markdown-preview/static/css/main.454ff70b.chunk.css"
+  },
+  {
+    "revision": "180004a84aeec60a27acd862b8893eee",
+    "url": "/fcc-markdown-preview/index.html"
+  }
+];
